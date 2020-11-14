ea5ab2b8915a2ad7400b7b72a2303a438756b6e3
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-14 14:28:14 +0600

    Admin Dashboard Created

--- client/web/src/app/admin/admin.module.ts
+++ client/web/src/app/admin/admin.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
   declarations: [AdminComponent, AdminDashboardComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MaterialModule
   ]
 })
 export class AdminModule { }
