5508dc0536b972caac3e03634c90839810b56404
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-15 04:21:23 +0600

    fix

--- client/web/src/app/ward-monitor/data-visualization/data-visualization.component.ts
+++ client/web/src/app/ward-monitor/data-visualization/data-visualization.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Chart} from 'chart.js';
+import * as Chart from 'chart.js';
 @Component({
   selector: 'app-data-visualization',
   templateUrl: './data-visualization.component.html',
