fd404116f1ac6db9134a576145766922f5886298
FAZLE RAKIB <fazle30@student.sust.edu>
2020-11-15 02:40:34 +0600

    Doctor Dashboard created & Ward-monitor Dashboard updated

--- client/web/src/app/ward-monitor/ward-monitor-routing.module.ts
+++ client/web/src/app/ward-monitor/ward-monitor-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { WardMonitorDashboardComponent } from './ward-monitor-dashboard/ward-monitor-dashboard.component';
 import { WardMonitorComponent } from './ward-monitor.component';
 import {DataVisualizationComponent} from './../ward-monitor/data-visualization/data-visualization.component';
-
+import {PatientAdmissionFormComponent} from './patient-admission-form/patient-admission-form.component';
 const routes: Routes = [
   {
     path: '',
@@ -18,7 +18,13 @@ const routes: Routes = [
         path: 'live-monitoring',
         pathMatch: 'full',
         component: DataVisualizationComponent
+      },
+      {
+        path: 'admit-patient',
+        pathMatch: 'full',
+        component: PatientAdmissionFormComponent
       }
+
     ]
   }
 ];
