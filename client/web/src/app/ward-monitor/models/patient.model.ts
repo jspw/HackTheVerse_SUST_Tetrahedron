49ee494a7e26ffc015a2912822692bb6afd3f847
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2020-11-15 04:18:17 +0600

    Patient Model Updated

--- client/web/src/app/ward-monitor/models/patient.model.ts
+++ client/web/src/app/ward-monitor/models/patient.model.ts
@@ -1,10 +1,15 @@
 export interface PatientData {
-    id: string,
-    bp: string,
+    _id: string
+    admitDate: string
     name: string,
-    temperature: string,
-    pulse: string,
-    primary_disease: string
+    age: number,
+    disease: string,
+    bed: string,
+    note: string
+    medics: Medicine[],
+    hospital: string,
+    ward: string,
+    sensorData: SensorData[]
 }
 
 export interface PatientAdmitData {
@@ -13,4 +18,31 @@ export interface PatientAdmitData {
     disease: string,
     bed: string,
     note: string
+    medics: [
+        {
+            name: string,
+            frequency: number[],
+            note: string
+        }
+    ],
+    // hospital: string,
+    // ward: string,
+}
+
+interface Medicine {
+    name: string,
+    frequency: number[],
+    note: string
+}
+
+interface SensorData {
+    name: string,
+    value: string[]
+}
+
+export interface AllPatientResponseData {
+    status: string,
+    data: {
+        patient: PatientData[]
+    }
 }
